879c30be700fb699e61a9801db9e0ee7992c5865
ana.sdp <septianadwip219@gmail.com>
2022-08-20 20:48:04 +0700

    Table view
    Tugas

--- Funios Group 7 TableView/Funios Group 7 TableView/CustomPeopleCell.swift
+++ Funios Group 7 TableView/Funios Group 7 TableView/CustomPeopleCell.swift
@@ -8,9 +8,24 @@
 import Foundation
 import UIKit
 
-class CustomPeopleCell: UITableViewCell {
+//class CustomPeopleCell: UITableViewCell {
+//    
+//    @IBOutlet weak var labelName: UILabel!
+//    @IBOutlet weak var buttonDelet: UIButton!
+//    @IBOutlet weak var imageViewPhoto: UIImageView!
+//}
+protocol Button {
+    func tap(name: String)
+    }
+
+class PersonTableViewCell: UITableViewCell {
     
     @IBOutlet weak var labelName: UILabel!
-    @IBOutlet weak var buttonDelet: UIButton!
+    @IBOutlet weak var buttonaction: UIButton!
     @IBOutlet weak var imageViewPhoto: UIImageView!
+    var delegate: Button!
+    
+    @IBAction func pressButton(_ sender: Any) {
+        delegate.tap(name: labelName.text!)
+    }
 }
--- Funios Group 7 TableView/Funios Group 7 TableView/ListViewController.swift
+++ Funios Group 7 TableView/Funios Group 7 TableView/ListViewController.swift
@@ -1,32 +1,86 @@
 //
-//  ListViewController.swift
-//  Funios Group 7 TableView
+//  ViewController.swift
+//  TabelView
 //
-//  Created by PT.Koanba on 29/07/22.
+//  Created by Mac Pro on 02/08/22.
 //
 
 import UIKit
 
-class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,
+    Button {
+    func tap(name: String) {
+//
+        let alertDialog = UIAlertController(title: name, message: "Kamu memilih \(name)", preferredStyle: .alert)
+        let actionButton = UIAlertAction(title: "OKE", style: .default)
+        let cancelButton = UIAlertAction(title: "Batal", style: .destructive)
+        alertDialog.addAction(actionButton)
+        alertDialog.addAction(cancelButton)
+
+        self.present(alertDialog, animated: true)
+        
+    }
+
+    var ListPerson: [Person] = []
     
-    @IBOutlet weak var listPersonTableView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view.
+        ListPerson = getDummyData ()
+     
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return ListPerson.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "CellPersonIdentifier")
-        cell.textLabel?.text = "Example"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonTableViewCell", for: indexPath) as! PersonTableViewCell
+        print("indexPath adalah \(indexPath)")
+        let person = ListPerson[indexPath.row]
+        cell.labelName.text = person.name
+        cell.imageViewPhoto.image = person.photo
+        cell.buttonaction.backgroundColor = .yellow
+        cell.buttonaction.layer.cornerRadius = 2
+        cell.delegate = self
         return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         1
     }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//        let person = ListPerson[indexPath.row]
+////
+//        let alertDialog = UIAlertController(title: person.name, message: "Kamu memilih \(person.name)", preferredStyle: .alert)
+//        let actionButton = UIAlertAction(title: "OKE", style: .default)
+//        let cancelButton = UIAlertAction(title: "Batal", style: .destructive)
+//        alertDialog.addAction(actionButton)
+//        alertDialog.addAction(cancelButton)
+//
+//        self.present(alertDialog, animated: true)
+//    }
+    func getDummyData () -> [Person] {
+        [Person(photo: UIImage(named: "imagePerson1")!, name: "Burhan"),
+         Person(photo: UIImage(named: "imagePerson2")!, name: "Santoso"),
+         Person(photo: UIImage(named: "imagePerson3")!, name: "Kelly"),
+         Person(photo: UIImage(named: "imagePerson4")!, name: "Jonat"),
+         Person(photo: UIImage(named: "imagePerson5")!, name: "Saihan"),
+         Person(photo: UIImage(named: "imagePerson1")!, name: "Burhan"),
+          Person(photo: UIImage(named: "imagePerson2")!, name: "Santoso"),
+          Person(photo: UIImage(named: "imagePerson3")!, name: "Kelly"),
+          Person(photo: UIImage(named: "imagePerson4")!, name: "Jonat"),
+          Person(photo: UIImage(named: "imagePerson5")!, name: "Saihan"),
+          Person(photo: UIImage(named: "imagePerson1")!, name: "Burhan"),
+           Person(photo: UIImage(named: "imagePerson2")!, name: "Santoso"),
+           Person(photo: UIImage(named: "imagePerson3")!, name: "Kelly"),
+           Person(photo: UIImage(named: "imagePerson4")!, name: "Jonat"),
+           Person(photo: UIImage(named: "imagePerson5")!, name: "Saihan"),
+        ]
+    }
 }
+
